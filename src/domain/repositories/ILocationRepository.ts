454800f9215d5d23f385e4b04003bb351bd4ccdc
hanseroland <rolandntougou@gmail.com>
2026-04-05 00:38:52 +0000

    feat: add IQRCodeRepository, IEmployeeInvitationRepository, ILocationRepository

---
+++ src/domain/repositories/ILocationRepository.ts
@@ -0,0 +1,8 @@
+import { Location } from "@domain/entities/Location";
+import { IBaseRepository } from "./IBaseRepository";
+
+export interface ILocationRepository extends IBaseRepository<Location> {
+
+    findByCompanyLocations(companyId: string): Promise<Location[]>;
+
+}
---
+++ src/domain/repositories/IQRCodeRepository.ts
@@ -0,0 +1,10 @@
+import { QRCode } from "@domain/entities/QRCode";
+import { IBaseRepository } from "./IBaseRepository";
+
+export interface IQRCodeRepository extends IBaseRepository<QRCode> {
+
+    findByCode(code: string): Promise<QRCode | null>;
+    findActive(locationId: string): Promise<QRCode[]>;
+    deactivateAllByLocation(locationId: string, companyId: string): Promise<void>;
+
+}
