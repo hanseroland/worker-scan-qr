13bdccbcbdc300c9668dd0df82f19050159f59a4
hanseroland <rolandntougou@gmail.com>
2026-04-07 01:16:39 +0000

    feat: add IPasswordService, ITokenService and IHashService interfaces

---
+++ src/domain/services/ITokenService.ts
@@ -0,0 +1,8 @@
+export interface ITokenService {
+  generateAccessToken(payload: object): string
+  generateRefreshToken(payload: object): string
+  generateActivationToken(): string        // crypto random token
+  generateResetPasswordToken(): string     // crypto random token
+  verifyAccessToken(token: string): object | null
+}
+
