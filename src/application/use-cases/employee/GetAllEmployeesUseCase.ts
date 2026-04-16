4e9f8b32f70c3847cb80be3b61ad9f67ce9e187a
hanseroland <rolandntougou@gmail.com>
2026-04-16 00:39:59 +0000

    feat: add SafeEmployeeDTO in GetAllEmployeeUseCase and UpdateEmployeeUseCase

--- src/application/use-cases/employee/GetAllEmployeesUseCase.ts
+++ src/application/use-cases/employee/GetAllEmployeesUseCase.ts
@@ -1,11 +1,21 @@
-import { Employee } from '@domain/entities/Employee';
 import { IEmployeeRepository } from '@domain/repositories/IEmployeeRepository';
+import { SafeEmployeeDTO } from '@shared/types/dto.types';
 
 export class GetAllEmployeesUseCase {
   constructor(private readonly employeeRepository: IEmployeeRepository) {}
 
-  async execute(companyId: string): Promise<Employee[]> {
+  async execute(companyId: string): Promise<SafeEmployeeDTO[]> {
     const employees = await this.employeeRepository.findAllByCompany(companyId);
-    return employees;
+    return employees.map(employee => {
+            const { 
+              userId: _us, 
+              employeeCode: _em, 
+              ...safeEmployee 
+            } = employee;
+            
+            return safeEmployee as SafeEmployeeDTO;
+      });
   }
+
+
 }
