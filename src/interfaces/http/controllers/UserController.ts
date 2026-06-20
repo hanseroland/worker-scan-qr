141a310b11c9548c14ed3ea826b5a2d5b271679c
hanseroland <rolandntougou@gmail.com>
2026-06-20 14:49:35 +0000

    feat: update authorization for getByCompanyIdUser controller

--- src/interfaces/http/controllers/UserController.ts
+++ src/interfaces/http/controllers/UserController.ts
@@ -2,8 +2,9 @@ import { DeleteUserUseCase } from "@application/use-cases/user/DeleteUserUseCase
 import { GetCompanyUsersUseCase } from "@application/use-cases/user/GetCompanyUsersUseCase";
 import { GetUserUseCase } from "@application/use-cases/user/GetUserUseCase";
 import { UpdateUserUseCase } from "@application/use-cases/user/UpdateUserUseCase";
+import { UserRole } from "@shared/enums";
 import { UpdateUserDTO } from "@shared/types/dto.types";
-import { NextFunction,Request,Response} from "express";
+import { NextFunction, Request, Response } from "express";
 
 export class UserController {
     constructor(
@@ -11,70 +12,84 @@ export class UserController {
         private getUserUseCase: GetUserUseCase,
         private updateUserUseCase: UpdateUserUseCase,
         private deleteUserUseCase: DeleteUserUseCase,
-    ){}
+    ) { }
 
-   
-   getById = async (req: Request<{id:string}>, res: Response, next: NextFunction) => {
-    try {
-        const result = await this.getUserUseCase.execute(
-            req.params.id,
-            req.user?.companyId || undefined
-        );
-        res.status(200).json(
-            {
-                success:true,
-                data:result
+
+    getById = async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.getUserUseCase.execute(
+                req.params.id,
+                req.user?.companyId || undefined
+            );
+            res.status(200).json(
+                {
+                    success: true,
+                    data: result
+                }
+            );
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    getByCompanyId = async (req: Request<{ companyId: string }>, res: Response, next: NextFunction) => {
+        try {
+
+            //Vérifier l'autorisation comme dans LocationController
+            const isSuperAdmin = req.user?.role === UserRole.SUPER_ADMIN;
+            const isCompanyAdminOwner =
+                req.user?.role === UserRole.COMPANY_ADMIN &&
+                req.user?.companyId === req.params.companyId;
+
+            if (!isSuperAdmin && !isCompanyAdminOwner) {
+                return res.status(403).json({
+                    success: false,
+                    message: "Unauthorized access"
+                });
             }
-        );
-    } catch (error) {
-        next(error);
-    }
-  }
 
-  getByCompanyId = async (req: Request<{companyId:string}>, res: Response, next: NextFunction) => {
-    try {
-        const result = await this.getCompanyUsersUseCase.execute(req.params.companyId);
-        res.status(200).json(
-            {
-                success:true,
-                data:result
-            }
-        );
-    } catch (error) {
-        next(error);
+            const result = await this.getCompanyUsersUseCase.execute(req.params.companyId);
+            res.status(200).json(
+                {
+                    success: true,
+                    data: result
+                }
+            );
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
 
-  
-  update = async (req: Request<{id:string}, {}, UpdateUserDTO>, res: Response, next: NextFunction)=>{
-    try {
-        const result = await this.updateUserUseCase.execute(
-            req.params.id,
-            req.body,
-            req.user?.companyId || undefined
-        );
-         res.status(200).json(
-            {
-                success:true,
-                data:result
-                
-            }
-          );
-    } catch (error) {
-        next(error);
-    }
-  }
 
-  delete = async (req: Request<{id:string}>, res: Response, next: NextFunction) => {
-    try {
-        await this.deleteUserUseCase.execute(
-            req.params.id,
-            req.user?.companyId || undefined
-        );
-         res.status(204).send()
-    } catch (error) {
-        next(error)
+    update = async (req: Request<{ id: string }, {}, UpdateUserDTO>, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.updateUserUseCase.execute(
+                req.params.id,
+                req.body,
+                req.user?.companyId || undefined
+            );
+            res.status(200).json(
+                {
+                    success: true,
+                    data: result
+
+                }
+            );
+        } catch (error) {
+            next(error);
+        }
     }
-  }
+
+    delete = async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+        try {
+            await this.deleteUserUseCase.execute(
+                req.params.id,
+                req.user?.companyId || undefined
+            );
+            res.status(204).send()
+        } catch (error) {
+            next(error)
+        }
+    }
 }
