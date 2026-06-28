e43735b29ce2e79d56b3efc4c00c6735e6920e36
hanseroland <rolandntougou@gmail.com>
2026-06-28 00:28:33 +0000

    feat: initialize createRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 // src/app.ts
 import express, { Application } from 'express';
+import { createRoutes } from '@interfaces/http/routes'
+import { controllers, jwtTokenService } from 'container';
 
 const app: Application = express();
 
 app.use(express.json());
+app.use('/api/v1/', createRoutes(controllers, jwtTokenService))
+
 
 export default app;
